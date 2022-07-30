b95ba74333b81e41f4c89c852ab2c7d057e386c3
tjdwns6872 <tjdwns6872@naver.com>
2022-07-31 00:27:16 +0900

    18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ baekjoon_Explanation/src/step_Io/explanation1.java
@@ -0,0 +1,10 @@
+package step_Io;
+import java.util.*;
+public class explanation1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		sc.close();
+		System.out.println(year-543);
+	}
+}
