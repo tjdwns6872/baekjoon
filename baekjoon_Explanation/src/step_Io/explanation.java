c96669af0efbd2d264267737feb2d2b9c0e8a84a
tjdwns6872 <tjdwns6872@naver.com>
2022-07-31 00:22:19 +0900

    10926 ??!

---
+++ baekjoon_Explanation/src/step_Io/explanation.java
@@ -0,0 +1,10 @@
+package step_Io;
+import java.util.*;
+public class explanation {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next();
+		System.out.println(id+"??!");
+	}
+
+}
