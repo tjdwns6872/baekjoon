a188f405335a254ca5c75af5bb55256c02eb2aa8
tjdwns6872 <tjdwns6872@naver.com>
2022-08-01 15:11:11 +0900

    2869 달팽이는 올라가고 싶다(시간초과)

---
+++ baekjoon_Explanation/src/setp_math1/explanation1.java
@@ -0,0 +1,27 @@
+package setp_math1;
+import java.util.*;
+public class explanation1 {
+	public static void main(String[] args) { 
+	//2869 달팽이는 올라가고 싶다
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(); //올라가는 미터
+		int b = sc.nextInt(); //미끄러지는 미터
+		int v = sc.nextInt(); //나무 막대 미터
+		int total = 0;
+		int day = 0;
+		while(true) {
+			day++;
+			total += a;
+			if(total >= v) {
+				break;
+			}
+			total -= b;
+		}/*
+		for(int total = 0; total < v; total+=a) {
+			day++;
+			total -= b;
+		}*/
+		sc.close();
+		System.out.println(day);
+	}
+}
