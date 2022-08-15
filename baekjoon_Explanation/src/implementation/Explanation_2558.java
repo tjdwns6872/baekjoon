b1e9351e2da38c1f6f6f475f6d2936ce5719f147
tjdwns6872 <tjdwns6872@naver.com>
2022-08-15 15:58:43 +0900

    2558 A+B -2

---
+++ baekjoon_Explanation/src/implementation/Explanation_2558.java
@@ -0,0 +1,10 @@
+package implementation;
+import java.util.*;
+public class Explanation_2558 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a+b);
+	}
+}
