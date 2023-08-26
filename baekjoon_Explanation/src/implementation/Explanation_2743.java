641a427834576b5a789d2689812f9a7a92adc456
tjdwns6872 <tjdwns6872@naver.com>
2023-08-26 17:02:26 +0900

    백준 2743 문제풀이

--- baekjoon_Explanation/src/implementation/Explanation_2743.java
+++ baekjoon_Explanation/src/implementation/Explanation_2743.java
@@ -4,10 +4,7 @@ public class Explanation_2743 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		String str = sc.next();
-		
 		System.out.println(str.length());
 	}
-
 }
