0939a0e5500765cda7083bf631c6cc3af98730ce
tjdwns6872 <tjdwns6872@naver.com>
2023-08-26 17:11:29 +0900

    백준 10757 문제풀이(BigInteger)

---
+++ baekjoon_Explanation/src/implementation/Explanation_10757.java
@@ -0,0 +1,13 @@
+package implementation;
+import java.math.BigInteger;
+import java.util.*;
+public class Explanation_10757 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger x = new BigInteger(sc.next());
+		BigInteger y = new BigInteger(sc.next());
+		System.out.println(x.add(y));
+		sc.close();
+	}
+}
