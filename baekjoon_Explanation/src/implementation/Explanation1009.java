e58386c7c3841ea8bc06aa1bb6599d71140b76a6
tjdwns6872 <tjdwns6872@naver.com>
2023-08-26 13:45:51 +0900

    .

--- baekjoon_Explanation/src/implementation/Explanation1009.java
+++ baekjoon_Explanation/src/implementation/Explanation1009.java
@@ -8,18 +8,24 @@ public class Explanation1009 {
 		int t = sc.nextInt();
 		int x;
 		int y;
-		Integer[] number = new Integer[t];
+		int sum = 1;
+		int[] number = new int[t];
 		for(int i = 0; i < t; i++) {
 			x = sc.nextInt();
 			y = sc.nextInt();
 			for(int z = 0; z < y; z++) {
-				x *= y;
+				System.out.println("x:"+x);
+				sum = sum*x;
+				System.out.println("sum"+sum);
 			}
-			System.out.println("x:"+x);
-			System.out.println("Integer.toString(x) : "+Integer.toString(x));
-			System.out.println("Integer.toString(x).charAt(Integer.toString(x).length()-1):"+Integer.toString(x).charAt(Integer.toString(x).length()-1));
-			System.out.println(Integer.valueOf(Integer.toString(x).charAt(Integer.toString(x).length()-1)));
-			number[i] = Integer.valueOf(Integer.toString(x).charAt(Integer.toString(x).length()-1));
+			int num = Character.getNumericValue(Integer.toString(sum).charAt(Integer.toString(x).length()-1));
+			if(num == 0) {
+				num = 10;
+			}
+			number[i] = num;
+			sum = 1;
+		}
+		for(int i = 0; i < t; i++) {
 			System.out.println(number[i]);
 		}
 	}
--- baekjoon_Explanation/src/implementation/Explanation_1100.java
+++ baekjoon_Explanation/src/implementation/Explanation_1100.java
@@ -4,18 +4,43 @@ import java.util.Scanner;
 
 public class Explanation_1100 {
 	public static void main(String[] args) {
-		String[][] chess = new String[8][8];
-		int count = 0;
+//		char[][] chess = new char[8][8];
+//		Scanner sc = new Scanner(System.in);
+//		int ck=0;
+//		
+//		for(int i = 0; i < chess.length; i++) {
+//			for(int j = 0; j < chess[0].length; j++) {
+//				chess[i][j] = sc.next().charAt(0);
+//			}
+//		}
+//		for(int i = 0; i < chess.length; i++) {
+//			for(int j = 0; j < chess[0].length; j++) {
+//				if((i == 0 || i % 2 == 0)&&(j == 0 || j % 2 == 0)) {
+//					if(chess[i][j] == 'F') {
+//						ck++;
+//					}
+//				}else if ((i % 2 != 0)&&(j % 2 != 0)) {
+//					if(chess[i][j] == 'F') {
+//						ck++;
+//					}
+//				}
+//			}
+//		}
+//		System.out.println(ck);
+		String[] chess = new String[8];
 		Scanner sc = new Scanner(System.in);
-		for(int i = 0; i < 8; i++) {
-			for(int j = 0; j < 8; j++) {
-				chess[i][j] = sc.next();
-				if(i == 0 || i % 2 == 0) {
-					if(j == 0 || j % 2 == 0) {
-						//count
-					}
-				}
+		int ck = 0;
+		for(int i = 0; i < chess.length; i++) {
+			for(int j = 0; j < chess[0].length(); j++) {
+				chess[i] = sc.next();
 			}
+			System.out.println(chess[i]);
 		}
 	}
 }
+
+
+
+
+
+
