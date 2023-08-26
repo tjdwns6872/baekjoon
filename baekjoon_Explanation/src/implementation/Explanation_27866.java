cbb88639150e025f1a5f951a8792b94809047f14
tjdwns6872 <tjdwns6872@naver.com>
2023-08-26 17:42:48 +0900

    백준 27866 문제 풀이

---
+++ baekjoon_Explanation/src/implementation/Explanation_27866.java
@@ -0,0 +1,12 @@
+package implementation;
+import java.util.*;
+public class Explanation_27866 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int x = sc.nextInt();
+		System.out.println(str.charAt(x-1));
+		sc.close();
+	}
+}
