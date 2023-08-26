d5a46c499eadea76c72306bc2ae6a92c6642fbdc
tjdwns6872 <tjdwns6872@naver.com>
2023-08-26 17:18:45 +0900

    백준 25314 문제 풀이

---
+++ baekjoon_Explanation/src/implementation/Explanation_25314.java
@@ -0,0 +1,14 @@
+package implementation;
+import java.util.*;
+public class Explanation_25314 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		for(int i = 0; i < x/4; i++) {
+			System.out.print("long ");
+		}
+		System.out.print("int");
+		sc.close();
+	}
+}
