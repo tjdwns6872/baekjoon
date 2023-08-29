f387c4260bb58b001e0fa95be281287280e498a9
tjdwns6872 <tjdwns6872@naver.com>
2023-08-29 22:18:20 +0900

    백준 2420 문제풀이(진행중)

---
+++ baekjoon_Explanation/src/implementation/Explanation_2420.java
@@ -0,0 +1,14 @@
+package implementation;
+import java.util.*;
+public class Explanation_2420 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int sum;
+		sum = x - y;
+		System.out.println(Math.abs(sum));
+		sc.close();
+	}
+}
