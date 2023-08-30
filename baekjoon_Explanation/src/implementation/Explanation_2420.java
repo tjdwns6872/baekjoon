430c6917aef8c3e22de0cc628de3a61807dcb3d2
tjdwns6872 <tjdwns6872@naver.com>
2023-08-30 23:21:53 +0900

    Scanner -> BufferedReader변경 int->Long로 변경

--- baekjoon_Explanation/src/implementation/Explanation_2420.java
+++ baekjoon_Explanation/src/implementation/Explanation_2420.java
@@ -1,14 +1,17 @@
 package implementation;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 public class Explanation_2420 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
-		int y = sc.nextInt();
-		int sum;
-		sum = x - y;
-		System.out.println(Math.abs(sum));
-		sc.close();
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		Long x = Long.parseLong(st.nextToken());
+		Long y = Long.parseLong(st.nextToken());
+		
+		System.out.println(Math.abs(x - y));
 	}
 }
