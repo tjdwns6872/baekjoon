09bc866c9f07d217d112ecf4b7bbe44388716253
tjdwns6872 <tjdwns6872@naver.com>
2023-10-23 20:41:29 +0900

    백준 3046문제풀이

---
+++ baekjoon_Explanation/src/implementation/Explanation_3046.java
@@ -0,0 +1,11 @@
+package implementation;
+import java.util.*;
+public class Explanation_3046 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r1 = sc.nextInt();
+		int ave = sc.nextInt();
+		System.out.println((ave*2)-r1);
+		sc.close();
+	}
+}
