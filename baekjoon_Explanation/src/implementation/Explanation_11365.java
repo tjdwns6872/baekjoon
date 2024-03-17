d25a770c8a7edcd4cf4a417d48160f6755979eac
tjdwns6872 <tjdwns6872@naver.com>
2024-03-18 00:03:01 +0900

    백준 11365 문제 풀이

---
+++ baekjoon_Explanation/src/implementation/Explanation_11365.java
@@ -0,0 +1,21 @@
+package implementation;
+
+import java.util.Scanner;
+
+public class Explanation_11365 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			String str = sc.nextLine();
+			if(str.equals("END")) {
+				break;
+			}
+			for(int i = (str.length()-1); i >= 0; i--) {
+				System.out.print(str.charAt(i));
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
