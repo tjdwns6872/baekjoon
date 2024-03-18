3a2eaa83f68ffbd39d2918f18d84dc8da1af180e
tjdwns6872 <tjdwns6872@naver.com>
2024-03-19 00:23:11 +0900

    백준 9316문제 풀이

---
+++ baekjoon_Explanation/src/implementation/Explanation_9316.java
@@ -0,0 +1,17 @@
+package implementation;
+
+import java.util.Scanner;
+
+public class Explanation_9316 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			System.out.println("Hello World, Judge "+i+"!");
+		}
+		sc.close();
+	}
+
+}
