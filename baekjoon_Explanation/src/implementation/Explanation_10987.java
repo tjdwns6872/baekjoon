cd8528157e68a45667f248be6da5156433770dd9
tjdwns6872 <tjdwns6872@naver.com>
2024-03-20 19:15:18 +0900

    백준 10987문제 풀이

---
+++ baekjoon_Explanation/src/implementation/Explanation_10987.java
@@ -0,0 +1,21 @@
+package implementation;
+
+import java.util.Scanner;
+
+public class Explanation_10987 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		str = str.toLowerCase();
+		int sum = 0;
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+				sum += 1;
+			}
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+
+}
