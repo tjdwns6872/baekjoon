7b00a9d4ff851b33f9d69201963d1428c02dcec6
tjdwns6872 <tjdwns6872@naver.com>
2024-03-31 01:47:37 +0900

    백준 1267문제 풀이

---
+++ baekjoon_Explanation/src/implementation/Explanation_1267.java
@@ -0,0 +1,27 @@
+package implementation;
+
+import java.util.Scanner;
+
+public class Explanation_1267 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int y = 0;
+		int m = 0;
+		for(int i = 0; i < n; i++) {
+			int call = sc.nextInt();
+			y += ((call/30)+1)*10;
+			m += ((call/60)+1)*15;
+		}
+		if(y == m) {
+			System.out.println("Y M "+y);
+		}else if(y > m) {
+			System.out.println("M "+m);
+		}else {
+			System.out.println("Y "+y);
+		}
+		sc.close();
+	}
+
+}
